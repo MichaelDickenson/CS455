cab287165effdb29e6940f20733aca41b32f6687
santiagofelix3 <santiagofelix3@gmail.com>
2017-03-27 21:38:35 -0600

    Layout

--- ONtheRoad/ONtheRoad/DashboardViewController.swift
+++ ONtheRoad/ONtheRoad/DashboardViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  DashboardViewController.swift
 //  ONtheRoad
 //
 //  Created by Santiago Félix Cárdenas on 2017-03-27.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class DashboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
