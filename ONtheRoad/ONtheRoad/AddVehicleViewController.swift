1168d03e768d9aeba21521edab97bec7e854767f
santiagofelix3 <santiagofelix3@gmail.com>
2017-03-29 21:02:33 -0600

    Camera and Photo Library working

--- ONtheRoad/ONtheRoad/AddVehicleViewController.swift
+++ ONtheRoad/ONtheRoad/AddVehicleViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AddVehicleViewController: UIViewController {
+class AddVehicleViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     //var vehiclesList = [VehicleProfile]()
     var vehicles: VehicleProfile?
@@ -35,7 +35,14 @@ class AddVehicleViewController: UIViewController {
     
     // MARK: Actions
     
+    @IBAction func imageGestureRecognizer(_ sender: UITapGestureRecognizer) {
+                
+        openActionSheet()
+    }
+    
     @IBAction func vehicleNameAction(_ sender: UITextField) {
+        vehicleName.resignFirstResponder()
+        
         if vehicleName.text! != "" {
             vehicleName.textColor = UIColor(red: 99/255.0, green: 175/255.0, blue: 213/255.0, alpha: 1.0)
             vehicleName.layer.borderColor = UIColor(red: 99/255.0, green: 175/255.0, blue: 213/255.0, alpha: 1.0).cgColor
@@ -45,37 +52,24 @@ class AddVehicleViewController: UIViewController {
     }
     
     @IBAction func makeSegueAction(_ sender: UITextField) {
-        resignFirstResponder()
-        //isEditing = false
         vehicleModel.allowsEditingTextAttributes = false
         
         self.performSegue(withIdentifier: "makeSegue", sender: nil)
     }
     
     @IBAction func modelSegueAction(_ sender: UITextField) {
-        
-        resignFirstResponder()
-        //isEditing = false
         vehicleModel.allowsEditingTextAttributes = false
         
         self.performSegue(withIdentifier: "modelSegue", sender: nil)
-        
-        /*func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         if let destinationViewController: ModelTableViewController = segue.destination as? ModelTableViewController {
-         //destinationViewController.vehiclesList = VehicleProfileData.vehicleData//vehiclesList// selectedMake = vehicleMake.text!
-         }
-         }*/
     }
     
     @IBAction func yearSegue(_ sender: UITextField) {
-        resignFirstResponder()
         vehicleYear.allowsEditingTextAttributes = false
         
         self.performSegue(withIdentifier: "yearSegue", sender: nil)
     }
     
     @IBAction func trimSegue(_ sender: UITextField) {
-        resignFirstResponder()
         vehicleTrim.allowsEditingTextAttributes = false
         
         self.performSegue(withIdentifier: "trimSegue", sender: nil)
@@ -87,7 +81,7 @@ class AddVehicleViewController: UIViewController {
                 returnedMake = "Acura"
             }
             vehicleMake.text = returnedMake
-            vehicles = VehicleProfile(name: vehicleName.text!, make: returnedMake, model: "", year: "", trim: "", type: "", id: "")
+            vehicles = VehicleProfile(photo: vehicleImage.image!, name: vehicleName.text!, make: returnedMake, model: "", year: "", trim: "", type: "", id: "")
             VehicleProfileData.vehicleData.append(vehicles!)
             //VehicleProfileData.vehicleData[0].make = returnedMake
 
@@ -102,7 +96,7 @@ class AddVehicleViewController: UIViewController {
         
         if let sourceViewController = sender.source as? ModelTableViewController, let returnedModel = sourceViewController.returnThis {
             vehicleModel.text = returnedModel
-            vehicles = VehicleProfile(name: vehicleName.text!, make: vehicleMake.text!, model: returnedModel, year: "", trim: "", type: "", id: "")
+            vehicles = VehicleProfile(photo: vehicleImage.image!, name: vehicleName.text!, make: vehicleMake.text!, model: returnedModel, year: "", trim: "", type: "", id: "")
             VehicleProfileData.vehicleData.append(vehicles!)
             //VehicleProfileData.vehicleData[0].model = returnedModel
             
@@ -117,7 +111,7 @@ class AddVehicleViewController: UIViewController {
         
         if let sourceViewController = sender.source as? YearTableViewController, let returnedYear = sourceViewController.returnThis {
             vehicleYear.text = returnedYear
-            vehicles = VehicleProfile(name: vehicleName.text!, make: vehicleMake.text!, model: vehicleModel.text!, year: returnedYear, trim: "", type: "", id: "")
+            vehicles = VehicleProfile(photo: vehicleImage.image!, name: vehicleName.text!, make: vehicleMake.text!, model: vehicleModel.text!, year: returnedYear, trim: "", type: "", id: "")
             VehicleProfileData.vehicleData.append(vehicles!)
             //VehicleProfileData.vehicleData[0].year = returnedYear
             
@@ -132,7 +126,7 @@ class AddVehicleViewController: UIViewController {
         
         if let sourceViewController = sender.source as? TrimTableViewController, let returnedTrim = sourceViewController.returnThis {
             vehicleTrim.text = returnedTrim
-            vehicles = VehicleProfile(name: vehicleName.text!, make: vehicleMake.text!, model: vehicleModel.text!, year: vehicleTrim.text!, trim: returnedTrim, type: "", id: "")
+            vehicles = VehicleProfile(photo: vehicleImage.image!, name: vehicleName.text!, make: vehicleMake.text!, model: vehicleModel.text!, year: vehicleTrim.text!, trim: returnedTrim, type: "", id: "")
             VehicleProfileData.vehicleData.append(vehicles!)
             //VehicleProfileData.vehicleData[0].trim = returnedTrim
             
@@ -152,6 +146,28 @@ class AddVehicleViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    // MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // Use edited representation of image.
+        guard let selectedImage = info[UIImagePickerControllerEditedImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        vehicleImage.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+
+    
     // MARK: Functions
     
     func setViewColors() {
@@ -162,4 +178,70 @@ class AddVehicleViewController: UIViewController {
         self.vehicleImage.layer.borderWidth = 2.0
         self.vehicleImage.layer.borderColor = UIColor(red: 99/255.0, green: 175/255.0, blue: 213/255.0, alpha: 1.0).cgColor
     }
+    
+    func openActionSheet() {
+        let optionMenu = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let openCamera = UIAlertAction(title: "Camera", style: .default, handler: {(alert:UIAlertAction!) -> Void in
+            self.openCamera()})
+        
+        let openLibrary = UIAlertAction(title: "Camera Roll", style: .default, handler: {(alert:UIAlertAction!) -> Void in
+            self.openPhotoLibrary()})
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        optionMenu.addAction(openCamera)
+        optionMenu.addAction(openLibrary)
+        optionMenu.addAction(cancelAction)
+        
+        self.present(optionMenu, animated: true, completion: nil)
+    }
+    
+    
+    func openCamera() {
+        
+        let alertController = UIAlertController(title: "Camera", message:
+            "The camera is not available", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+        
+        let src = UIImagePickerControllerSourceType.camera
+        
+        // Check if camera is available
+        guard UIImagePickerController.isSourceTypeAvailable(src)
+            else {
+                self.present(alertController, animated: true, completion: nil)
+                return
+        }
+        
+        guard let arr = UIImagePickerController.availableMediaTypes(for: src)
+            else {
+                return
+        }
+        
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.sourceType = src
+        imagePicker.mediaTypes = arr
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func openPhotoLibrary() {
+        
+        //Let user pick media
+        let imagePickerController = UIImagePickerController()
+        
+        //Do not allow photos to be taken, only picked from media library
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.allowsEditing = true
+        
+        //Notify view controller that an image is picked
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
 }
+
+
+
+
